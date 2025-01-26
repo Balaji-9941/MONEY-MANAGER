f21f25dd1c2723cac8a2d6789e58f4cbdd5a073d
Balaji-9941 <manoharlalitha9941@gmail.com>
2025-01-26 11:41:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,42 @@ const state = {
 };
 const container = document.querySelector(".sect");
 const htmlSection = ({ income, expense, total, debt }) => `
-        <p class="income mt-5">INCOME :  ₹${income}</p>
-        <p class="expense">EXPENSE :  ₹${expense} </p>
-        <p class="total">TOTAL :  ₹${total} </p>
-        <p class="debt">DEBT(Loan):  ₹${debt}</p>
+     <div class="sect_part_1">
+        <p class="income_txt">INCOME : ₹${income}</p>
+        <button class="inc_button">
+          <i class="fa-solid fa-plus"></i>Add income
+        </button>
+        <button class="inc_button_2" style="background-color: greenyellow">
+          <i class="fa-solid fa-plus"></i>
+        </button>
+      </div>
+      <div class="sect_part_1">
+        <p class="expense_txt">EXPENSE : ₹${expense}</p>
+        <button class="exp_button">
+          <i class="fa-solid fa-plus"></i>Add expense
+        </button>
+        <button class="exp_button_2" style="background-color: red">
+          <i class="fa-solid fa-plus"></i>
+        </button>
+      </div>
+      <div class="sect_part_1">
+        <p class="debt_txt">LOAN: ₹${debt}</p>
+        <button class="debt_button">
+          <i class="fa-solid fa-plus"></i>Add Loan
+        </button>
+        <button class="debt_button_2" style="background-color: darkkhaki">
+          <i class="fa-solid fa-plus"></i>
+        </button>
+      </div>
+      <div class="sect_part_1">
+        <p class="total_txt">TOTAL : ₹${total}</p>
+      </div>
 `;
 const loadInitial = () => {
   const income = 0;
   const expense = 0;
   const total = 0;
-  const debt = 0;
+  const debt = 100000000;
   container.insertAdjacentHTML(
     "beforebegin",
     htmlSection({ income, expense, total, debt })
